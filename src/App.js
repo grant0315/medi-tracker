07c72d3a8ac0037aa57c30112c253a4845d7f4bf
Grant <granthopkins0315@gmail.com>
2021-03-04 17:14:22 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import BottomNav from 'components/bottom-nav.js';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <BottomNav></BottomNav>
     </div>
   );
 }
---
+++ src/components/bottom-nav.js
@@ -0,0 +1,11 @@
+import React, { component } from 'react';
+
+export default class BottomNav extends React.Component {
+    render() {
+        <ul>
+            <li>Timer</li>
+            <li>Log</li>
+            <li>Calender</li>
+        </ul>
+    }
+}
